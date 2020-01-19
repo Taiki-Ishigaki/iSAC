5d3b7efaf4794d8d91176ebbbed63505c903c775
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2020-01-19 21:05:06 +0900

    [change] for joystick

--- choreonoid/simplecontroller/tank_controller.cpp
+++ choreonoid/simplecontroller/tank_controller.cpp
@@ -1,21 +1,28 @@
 #include <cnoid/SimpleController>
+#include <cnoid/Joystick>
 
 using namespace cnoid;
 
-class TankController : public SimpleController
+class tank_controller : public SimpleController
 {
-    Link* joint;
-    double q_ref;
-    double q_prev;
+    Link* joints[2];
+    double q_ref[2];
+    double q_prev[2];
     double dt;
+    Joystick joystick;
 
 public:
     virtual bool initialize(SimpleControllerIO* io) override
     {
-        joint = io->body()->link("TURRET_P");
-        joint->setActuationMode(Link::JOINT_TORQUE);
-        io->enableIO(joint);
-        q_ref = q_prev = joint->q();
+        joints[0] = io->body()->link("TURRET_Y");
+        joints[1] = io->body()->link("TURRET_P");
+
+        for(int i=0; i < 2; ++i){
+            Link* joint = joints[i];
+            joint->setActuationMode(Link::JOINT_TORQUE);
+            io->enableIO(joint);
+            q_ref[i] = q_prev[i] = joint->q();
+        }
 
         dt = io->timeStep();
 
@@ -24,18 +31,31 @@ public:
 
     virtual bool control() override
     {
-        // PD gains
         static const double P = 200.0;
         static const double D = 50.0;
+        static const int axisID[] = { 2, 3 };
 
-        double q = joint->q(); // input
-        double dq = (q - q_prev) / dt;
-        double dq_ref = 0.0;
-        joint->u() = P * (q_ref - q) + D * (dq_ref - dq); // output
-        q_prev = q;
+        joystick.readCurrentState();
+
+        for(int i=0; i < 2; ++i){
+            Link* joint = joints[i];
+            double q = joint->q();
+            double dq = (q - q_prev[i]) / dt;
+            double dq_ref = 0.0;
+
+            double pos = joystick.getPosition(axisID[i]);
+            if(fabs(pos) > 0.25){
+                double deltaq = 0.002 * pos;
+                q_ref[i] += deltaq;
+                dq_ref = deltaq / dt;
+            }
+
+            joint->u() = P * (q_ref[i] - q) + D * (dq_ref - dq);
+            q_prev[i] = q;
+        }
 
         return true;
     }
 };
 
-CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(TankController)
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(tank_controller)
